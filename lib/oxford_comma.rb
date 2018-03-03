043cfefe9c30ce68c7e61064b91ea19b2d506ccc
Leann Kim <leannminakim@gmail.com>
2018-03-03 20:04:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    "array[0] + , + array[1] + and + array[2]"
+    array[0] + "," + array[1] + " and " + array[2]
   elsif array.size > 3
     array[0..-2].split(",") + " and " + array[-1]
   end
