2aeb573c9f1a18e37d1bfe1e525e9b50f6fda92c
Leann Kim <leannminakim@gmail.com>
2018-03-03 20:09:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.size == 3
     array[0] + ", " + array[1] + ", and " + array[2]
   elsif array.size > 3
-    array[-1] = "last"
-    array.pop << ", and "" << "last"
+    array[-1] = last
+    array.pop << ", and "" << last
   end
 end
