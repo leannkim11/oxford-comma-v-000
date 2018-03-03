286fc0718aedc859ebfa856605138007cadd4c23
Leann Kim <leannminakim@gmail.com>
2018-03-03 20:02:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 1
     array.join
   elsif array.size == 2
-    array[0] + "and" + array[1]
+    array[0] + " and " + array[1]
   elsif array.size == 3
     "array[0] + , + array[1] + and + array[2]"
   elsif array.size > 3
