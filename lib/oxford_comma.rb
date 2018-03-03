80946f09907946ec0781afa6e0198e8ff1fcb817
Leann Kim <leannminakim@gmail.com>
2018-03-03 20:09:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,6 +7,6 @@ def oxford_comma(array)
     array[0] + ", " + array[1] + ", and " + array[2]
   elsif array.size > 3
     array[-1] = last
-    array.pop << ", and  << last
+    array.pop << ", and " << last
   end
 end
